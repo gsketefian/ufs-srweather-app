7524fe57ae0987e8f86b033956aeb2875f182f64
gerard ketefian <gerard.ketefian@noaa.gov>
2024-02-24 00:12:38 +0000

    Add ability to (1) save preexisting output directory under a different name as a backup (via new argument "preexisting_dir_method"); (2) create a subdirectory under the output directory in which all the images generated appear (when listed alphabetically) in the order in which they appear in the yaml plot configuration file )via the new argument "create_ordered_plots").  Also, keep track of the number of images (plots) successfully generated and clean up the way some of the output messages are printed out.

--- ush/metviewer/make_mv_vx_plots.py
+++ ush/metviewer/make_mv_vx_plots.py
@@ -2,6 +2,8 @@
 
 import os
 import sys
+import shutil
+from datetime import datetime
 import glob
 import argparse
 import yaml
@@ -36,9 +38,9 @@ def make_mv_vx_plots(args):
     """
 
     # Set up logging.
-    # If the name/path of a log file has been specified in the command line arguments,
-    # place the logging output in it (existing log files of the same name are overwritten).
-    # Otherwise, direct the output to the screen.
+    # If the name/path of a log file has been specified in the command line
+    # arguments, place the logging output in it (existing log files of the
+    # same name are overwritten).  Otherwise, direct the output to the screen.
     log_level = str.upper(args.log_level)
     FORMAT = "[%(levelname)s:%(name)s:  %(filename)s, line %(lineno)s: %(funcName)s()] %(message)s"
     if args.log_fp:
@@ -58,16 +60,54 @@ def make_mv_vx_plots(args):
 
     fcst_init_info = config_dict['fcst_init_info']
     fcst_init_info = map(str, list(fcst_init_info.values()))
-    # fcst_init_info is a list containing both strings and integers.  For use below,
-    # convert it to a list of strings only.
+    # fcst_init_info is a list containing both strings and integers.  For
+    # use below, convert it to a list of strings only.
     fcst_init_info = [str(elem) for elem in fcst_init_info]
     fcst_len_hrs = str(config_dict['fcst_len_hrs'])
 
-    # Keep track of the number of times the script that generates a 
-    # MetViewer xml and calls MetViewer is called.  This can then be
-    # compared to the number of plots (png files) generated to see
-    # if any are missing/failed.
+    # Check if output directory exists and take action according to how the
+    # args.preexisting_dir_method flag is set.
+    if os.path.exists(args.output_dir):
+        if args.preexisting_dir_method == 'rename':
+            now = datetime.now()
+            renamed_dir = args.output_dir + now.strftime('.old_%Y%m%d_%H%M%S')
+            logging.info(dedent(f'''\n
+                Output directory already exists:
+                  {args.output_dir}
+                Moving (renaming) preexisting directory to:
+                  {renamed_dir}'''))
+            os.rename(args.output_dir, renamed_dir)
+        elif args.preexisting_dir_method == 'delete':
+            logging.info(dedent(f'''\n
+                Output directory already exists:
+                  {args.output_dir}
+                Removing existing directory...'''))
+            shutil.rmtree(args.output_dir)
+        else:
+            raise FileExistsError(dedent(f'''\n
+                Output directory already exists:
+                  {args.output_dir}
+                Stopping.'''))
+
+    # If the flag create_ordered_plots is set to True, create (if it doesn't
+    # already exist) a new directory in which we will store copies of all
+    # the images (png files) that METviewer will generate such that the
+    # images are ordered via an index in their name.  This allows a pdf to
+    # quickly be created from this directory (e.g. using tools available in
+    # Adobe Acrobat) that contains all the plots in the order they were
+    # listed in the yaml plot configuration file that this script reads in.
+    if args.create_ordered_plots:
+        ordered_plots_dir = os.path.join(args.output_dir, 'ordered_plots')
+        Path(ordered_plots_dir).mkdir(parents=True, exist_ok=True)
+
+    # Initialze (1) the counter that keeps track of the number of times the
+    # script that generates a MetViewer xml and calls MetViewer is called and
+    # (2) the counter that keeps track of the number of images (png files)
+    # that were successfully generated.  Each call to the script should
+    # generate an image, so these two counters can be compared at the end to
+    # see how many images were (not) successfully generated.
     num_mv_calls = 0
+    num_images_generated = 0
 
     vx_stats_dict = config_dict["vx_stats"]
     for stat, stat_dict in vx_stats_dict.items():
@@ -81,27 +121,42 @@ def make_mv_vx_plots(args):
 
         elif (not args.include_stats) or (args.include_stats and stat in args.include_stats):
             logging.info(dedent(f"""
-                Plotting statistic "{stat}" for various forecast variables ...
+                Plotting statistic "{stat}" for various forecast fields ...
                 """))
-            logging.debug(f"""\nstat_dict =\n{get_pprint_str(stat_dict, '  ')}\n""")
+            msg = dedent(f"""
+                Dictionary of fields, levels, and thresholds (if applicable) for this statistic is:
+                  stat_dict = """)
+            indent_str = ' '*(5 + len('stat_dict'))
+            msg = msg + get_pprint_str(stat_dict, indent_str).lstrip()
+            logging.debug(msg)
 
-            for fcst_var, fcst_var_dict in stat_dict.items():
+            for fcst_field, fcst_field_dict in stat_dict.items():
 
                 logging.info(dedent(f"""
-                    Plotting statistic "{stat}" for forecast variable "{fcst_var}" at various levels ...
+                    Plotting statistic "{stat}" for forecast field "{fcst_field}" at various levels ...
                     """))
-                logging.debug(f"""\nfcst_var_dict =\n{get_pprint_str(fcst_var_dict, '  ')}\n""")
+                msg = dedent(f"""
+                    Dictionary of levels and thresholds (if applicable) for this field is:
+                      fcst_field_dict = """)
+                indent_str = ' '*(5 + len('fcst_field_dict'))
+                msg = msg + get_pprint_str(fcst_field_dict, indent_str).lstrip()
+                logging.debug(msg)
 
-                for level, level_dict in fcst_var_dict.items():
+                for level, level_dict in fcst_field_dict.items():
                     logging.info(dedent(f"""
-                        Plotting statistic "{stat}" for forecast variable "{fcst_var}" at level "{level}" ...
+                        Plotting statistic "{stat}" for forecast field "{fcst_field}" at level "{level}" ...
                         """))
-                    logging.debug(f"""\nlevel_dict =\n{get_pprint_str(level_dict, '  ')}\n""")
+                    msg = dedent(f"""
+                        Dictionary of thresholds (if applicable) for this level is:
+                          level_dict = """)
+                    indent_str = ' '*(5 + len('level_dict'))
+                    msg = msg + get_pprint_str(level_dict, indent_str).lstrip()
+                    logging.debug(msg)
 
                     thresholds = level_dict['thresholds']
                     for thresh in thresholds:
                         logging.info(dedent(f"""
-                            Plotting statistic "{stat}" for forecast variable "{fcst_var}" at level "{level}"
+                            Plotting statistic "{stat}" for forecast field "{fcst_field}" at level "{level}"
                             and threshold "{thresh}" (threshold may be empty for certain stats) ...
                             """))
 
@@ -111,26 +166,59 @@ def make_mv_vx_plots(args):
                                   + ['--vx_stat', stat,
                                      '--fcst_init_info'] + fcst_init_info \
                                   + ['--fcst_len_hrs', fcst_len_hrs, 
-                                     '--fcst_var', fcst_var,
+                                     '--fcst_field', fcst_field,
                                      '--level_or_accum', level,
                                      '--threshold', thresh, 
                                      '--mv_output_dir', args.output_dir]
 
-                        logging.debug(f"""\nArgument list passed to plotting script is:\nargs_list =\n{get_pprint_str(args_list, '  ')}\n""")
+                        msg = dedent(f"""
+                            Argument list passed to plotting script is:
+                              args_list = """)
+                        indent_str = ' '*(5 + len('args_list'))
+                        msg = msg + get_pprint_str(args_list, indent_str).lstrip()
+                        logging.debug(msg)
 
                         num_mv_calls += 1
                         logging.debug(dedent(f"""
                             Calling MetViewer plotting script ...
                               num_mv_calls = {num_mv_calls}
                             """))
-                        plot_vx_metviewer(args_list)
+                        output_xml_fp = plot_vx_metviewer(args_list)
                         logging.debug(dedent(f"""
                             Done calling MetViewer plotting script.
                             """))
 
+                        # Keep track of the number of images that are successfully created.
+                        #
+                        # First, use the absolute path to the xml file created to generate the
+                        # path to and name of the image that should have been created.
+                        output_image_fp = os.path.splitext(output_xml_fp)[0] + '.' + 'png'
+                        output_image_fn = os.path.basename(output_image_fp)
+                        # If the image file exists, increment the count of successfully created
+                        # images.
+                        if os.path.isfile(output_image_fp):
+                            num_images_generated += 1
+
+                        # If the image was successfully created and args.create_ordered_plots
+                        # is set True, make a copy of the image in a designated subdirectory
+                        # that will contain renamed versions of the images such that their 
+                        # alphabetical order corresponds to the order in which they appear in
+                        # the yaml plot configuration file.
+                        if os.path.isfile(output_image_fp) and args.create_ordered_plots:
+                            # Generate the name of/path to a copy of the image file such that this
+                            # name contains an index used for alphabetically ordering the files.
+                            # This ordering is useful when creating a presentation, e.g. a pdf file,
+                            # from the images.
+                            output_image_fn_ordered = '_'.join([f'p{num_mv_calls:03}', output_image_fn])
+                            output_image_fp_ordered = os.path.join(ordered_plots_dir, output_image_fn_ordered)
+                            # Copy and rename the image.
+                            shutil.copy(output_image_fp, output_image_fp_ordered)
+
     logging.info(dedent(f"""
         Total number of calls to MetViewer plotting script:
           num_mv_calls = {num_mv_calls}
+        Total number of image files generated:
+          num_images_generated = {num_images_generated}
         """))
 #
 # -----------------------------------------------------------------------
@@ -145,19 +233,18 @@ if __name__ == "__main__":
         description='Call MetViewer to create vx plots.'
     )
 
-    # Find the path to the directory containing the clone of the SRW App.  The index of
-    # .parents will have to be changed if this script is moved elsewhere in the SRW App's
-    # directory structure.
+    # Find the path to the directory containing the clone of the SRW App.
+    # The index of .parents will have to be changed if this script is moved
+    # elsewhere in the SRW App's directory structure.
     crnt_script_fp = Path(__file__).resolve()
     home_dir = crnt_script_fp.parents[2]
     expts_dir = Path(os.path.join(home_dir, '../expts_dir')).resolve()
     parser.add_argument('--output_dir',
                         type=str,
                         required=False, default=os.path.join(expts_dir, 'mv_output'),
-                        help=dedent(f'''Directory in which to place output files (generated xmls,
-                                        MetViewer generated plots and other files, etc).  These
-                                        will usually be placed in subdirectories under this 
-                                        output directory.'''))
+                        help=dedent(f'''Base directory in which to place output files (generated xmls,
+                                        MetViewer generated plots, log files, etc).  These will usually
+                                        be placed in subdirectories under this output directory.'''))
 
     parser.add_argument('--config_fp',
                         type=str,
@@ -192,6 +279,22 @@ if __name__ == "__main__":
                         required=False, default=[],
                         choices=['auc', 'bias', 'brier', 'fbias', 'rely', 'rhist', 'ss'],
                         help='Stats to exclude from verification plot generation.')
+
+    parser.add_argument('--preexisting_dir_method',
+                        type=str.lower,
+                        required=False, default='rename',
+                        choices=['rename', 'delete', 'quit'], 
+                        help=dedent(f'''Method for dealing with pre-existing output directories.'''))
+
+    parser.add_argument('--create_ordered_plots',
+                        required=False, action=argparse.BooleanOptionalAction,
+                        help=dedent(f'''Boolean flag for creating a directory that contains copies of
+                                        all the generated images (png files) and renamed such that they
+                                        are alphabetically in the same order as the user has specified
+                                        in the yaml plot configuration file (the one passed to the optional
+                                        --config_fp argument).  This is useful for creating a pdf of 
+                                        the plots from the images that includes the plots in the same 
+                                        order as in the plot configuration file.'''))
 
     args = parser.parse_args()
 
