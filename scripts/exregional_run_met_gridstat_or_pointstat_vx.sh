435a9307940684f73114172c307150d1a99b3318
gerard ketefian <gerard.ketefian@noaa.gov>
2024-11-29 18:59:34 -0700

    Second set of changes from branch feature/daily_obs_tasks_hrrrcast into branch feature/daily_obs_tasks_hrrrcast_trial_mods.

--- scripts/exregional_run_met_gridstat_or_pointstat_vx.sh
+++ scripts/exregional_run_met_gridstat_or_pointstat_vx.sh
@@ -133,9 +133,9 @@ else
 # Note that ACCUM_HH will not be defined for the REFC, RETOP, SFC, and
 # UPA field groups.
   set_vx_params \
-    obtype="${OBTYPE}" \
     field_group="${FIELD_GROUP}" \
     accum_hh="${ACCUM_HH:-}" \
+    outvarname_obtype="obtype" \
     outvarname_grid_or_point="grid_or_point" \
     outvarname_fieldname_in_MET_filedir_names="FIELDNAME_IN_MET_FILEDIR_NAMES"
 
@@ -272,7 +272,7 @@ STAGING_DIR="${OUTPUT_BASE}/stage/${FIELDNAME_IN_MET_FILEDIR_NAMES}"
 #
 #-----------------------------------------------------------------------
 #
-case "$OBTYPE" in
+case "${obtype}" in
   "CCPA"|"NOHRSC")
     vx_intvl="$((10#${ACCUM_HH}))"
     vx_hr_start="${vx_intvl}"
@@ -440,7 +440,7 @@ settings="\
 # Field information.
 #
 'fieldname_in_met_filedir_names': '${FIELDNAME_IN_MET_FILEDIR_NAMES}'
-'obtype': '${OBTYPE}'
+'obtype': '${obtype}'
 'accum_hh': '${ACCUM_HH:-}'
 'accum_no_pad': '${ACCUM_NO_PAD:-}'
 'metplus_templates_dir': '${METPLUS_CONF:-}'
